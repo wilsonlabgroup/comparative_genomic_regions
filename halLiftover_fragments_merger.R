620ae174f2622b8a019231c5fdd376bdad9368e9
hy09 <huayunhou@gmail.com>
2024-12-13 15:04:45 -0500

    changed parameters min/max_frac to handle either fraction or length

--- halLiftover_fragments_merger.R
+++ halLiftover_fragments_merger.R
@@ -2,7 +2,7 @@
 # simple method to cancatenate halliftover outputs
 
 # Merge hal liftover output fragments that are within certain bps (controled by --gap). If fragments are on multiple chromosomes treat each chromosome separately
-# After merging, filter the merged fragmenets by length relative to original query peaks (controled by --max_perc and --min_perc)
+# After merging, filter the merged fragmenets by length relative to original query peaks (controled by --max_frac and --min_frac)
 # If a mapped summit file is given, further filter the merged regions for ones that overlapped corresponding mapped summit files  
 
 suppressPackageStartupMessages(library(GenomicRanges))
@@ -16,10 +16,10 @@ option_list <- list(
   make_option(c("-t","--tFile"), help = "Input mapped bed file"),
   make_option(c("-o","--oFile"), help = "Output file"),
   make_option(c("-s","--sFile"), help = "input mapped-summit bed file"),
-  make_option("--max_perc",default = 125, help = "Maximum percentage of clustered regions/peaks width, Default 125 (%)"),
-  make_option("--min_perc", default = 75, help = "Minimum percentage of clustered regions/peaks width, Default 25 (%)"),
-  make_option("--max_len", help = "Maximum length of clustered regions/peaks width"),
-  make_option("--min_len", default = 75, help = "Minimum length of clustered regions/peaks width"),
+  make_option("--max_frac",default = 1.25, help = "Maximum fraction of clustered regions/peaks width, Default 125 (%). If >10, treated as maximum length (bp)"),
+  make_option("--min_frac", default = 0.75, help = "Minimum fraction of clustered regions/peaks width, Default 25 (%). If >10, treated as minumum length (bp)"),
+  #make_option("--max_len", help = "Maximum length of clustered regions/peaks width"),
+  #make_option("--min_len", default = 75, help = "Minimum length of clustered regions/peaks width"),
   make_option("--gap", default = 50, help = "Gap width. Fragments within distance < gap width will be merged")
 )
 
@@ -32,27 +32,26 @@ tFile <- args$tFile
 oFile <- args$oFile
 
 MAX_GAP <- args$gap
+MIN_FRAC <- args$min_frac
+MAX_FRAC <- args$max_frac
 
 print(args)
 
-# if min/max length parameters is given, use the fixed parameters instead 
-if(length(intersect(c("max_len","min_len"),names(args))) == 2){
+# if either min/max frac is > 10 , treat it as min/max length
+if(MIN_FRAC > 10 | MAX_FRAC > 10){
   filter_by <- "length"
-  MAX_LEN <- args$max_len
-  MIN_LEN <- args$min_len
+  MAX_LEN <- args$max_frac
+  MIN_LEN <- args$min_frac
   print(paste0("Filter merged regions by fixed length, between ", MIN_LEN, " and ", MAX_LEN))
-} 
-
-if(length(intersect(c("min_perc","max_perc"),names(args))) == 2){
+} else{
   filter_by <- "frac"
-  MIN_PERC <- args$min_perc
-  MAX_PERC <- args$max_perc
-  print(paste0("Filter merged regions by relative fraction of peaks, between ", MIN_PERC, " and ", MAX_PERC))
-}
+  print(paste0("Filter merged regions by relative fraction of original peaks, between ", MIN_FRAC, " and ", MAX_FRAC))
+}  
 
 # check if mapped summit files exist
 if(!"sFile" %in% names(args)){
   print("No mapped summits given, ignore summit overlapping requirement")
+  use_summit <- FALSE
 } else{
   sFile <- args$sFile
   use_summit <- TRUE
@@ -62,8 +61,8 @@ if(!"sFile" %in% names(args)){
 ############################################################
 # # for testing
 # MAX_GAP = 50
-# MIN_PERC = 25
-# MAX_PERC = 125
+# MIN_FRAC = 0.75
+# MAX_FRAC = 1.25
 #  
 # setwd("~/mdwilson/huayun/test/test_hal_50bp//")
 # qFile <- "../test_halper/maec_rela_peaks.bed" #original peak file
@@ -103,7 +102,7 @@ targets_merged_df$mapped_peakname <- sapply(strsplit(peaknames, "\\."),"[[",1)
 targets_merged_df$peak_width <- peak_width[targets_merged_df$mapped_peakname]
 
 # calculate percentage of mapped region clusters / peak width
-targets_merged_df$perc <- with(targets_merged_df, 100*(width/peak_width))
+targets_merged_df$perc <- with(targets_merged_df, width/peak_width)
 
 # if mapped summit file exist
 
@@ -125,9 +124,9 @@ if(use_summit){
 # filter merged regions based on size (> merging gap threshold and within a certain proportion of the original peak) or by merged width
 
 if(filter_by == "frac"){
-  targets_merged_df_fil <- subset(targets_merged_df, perc >= MIN_PERC & perc <= MAX_PERC)
+  targets_merged_df_fil <- subset(targets_merged_df, perc >= MIN_FRAC & perc <= MAX_FRAC)
 } else{
-  targets_merged_df_fil <- subset(targets_merged_df, peak_width >= MIN_LEN & peak_width <= MAX_PERC)
+  targets_merged_df_fil <- subset(targets_merged_df, peak_width >= MIN_LEN & peak_width <= MAX_FRAC)
 }
 
 
